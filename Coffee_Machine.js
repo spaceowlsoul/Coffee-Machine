e37f8c2a8be0009977e8337ea448ac6494595363
spaceowlsoul <yuliasunny007@gmail.com>
2022-08-19 14:47:01 +0300

    implement the possibility to add syrups

--- Coffee_Machine.js
+++ Coffee_Machine.js
@@ -1,4 +1,3 @@
-
 const input = require('sync-input');
 
 let supply = {
@@ -6,8 +5,18 @@ let supply = {
     milk: 540, 
     beans: 120, 
     cups: 9, 
-    money: 550,
+    money: 550,   
 };
+
+const syrups = [
+    {name: 'hazelnut', amount: 200},
+    {name: 'almond', amount: 200},
+    {name: 'vanilla', amount: 100},
+    {name: 'no syrup', amount: 0},
+];
+
+const syrupsPrice = 2;
+const syrupsPerServingMl = 10;
     
 const beverages = [
     {name: 'espresso', water: 250, milk: 0, beans: 16, price: 4},
@@ -21,10 +30,13 @@ function greetingMessage() {
     ${supply.milk} ml of milk
     ${supply.beans} g of coffee beans
     ${supply.cups} disposable cups
+    ${syrups[0].amount} ml of hazelnut syrup
+    ${syrups[1].amount} ml of almond syrup
+    ${syrups[2].amount} ml of vanilla syrup
     $${supply.money} of money`);
 }
 
-function checkSupplies(typeOfCoffee) {
+function checkSupplies(typeOfCoffee, selectedSyrup) {
     if (supply.cups < 1) {
         console.log('Sorry, not enough cups!');
         return false;
@@ -36,20 +48,29 @@ function checkSupplies(typeOfCoffee) {
         return false;
     } else if (supply.beans - beverages[typeOfCoffee].beans < 0) {
         console.log('Sorry, not enough beans!');
-        return false;
+        return false; 
+    } else if (syrups[selectedSyrup].amount - syrupsPerServingMl < 0) {
+        if (selectedSyrup !== syrups.findIndex(el => el.name === 'no syrup')) { 
+            console.log('Sorry, not enough syrup!');
+            return false; 
+        } else {
+            return true;
+        }
     } else {
         return true;
     }
 }
 
-function makingCoffee(beverage) {
-    if (checkSupplies(beverage)) {
+function makingCoffee(beverage, chosenSyrup) {
+    if (checkSupplies(beverage, chosenSyrup)) {
         console.log('I have enough resources, making you a coffee! \n');
         supply.water -= beverages[beverage].water;
         supply.milk -= beverages[beverage].milk;
         supply.beans -= beverages[beverage].beans;
+        syrups[chosenSyrup].amount -= syrupsPerServingMl;
         supply.cups -= 1;
         supply.money += beverages[beverage].price;
+        supply.money += syrupsPrice;
     }
 }
 
@@ -59,7 +80,18 @@ function buy() {
         return;
     }
     const beverage = Number(choice) - 1;
-    makingCoffee(beverage);
+    const answerOne = input('Would you like to add syrup? Choose "yes" or "no".');
+    if (answerOne.toLowerCase() === 'no') {
+        makingCoffee(beverage, syrups.findIndex(el => el.name === 'no syrup'));
+        return;
+    } 
+    const answerTwo = input(`What kind of syrup would you like to add?
+        1 - hazelnut, 2 - almond, 3 - vanilla or back - to main menu: \n'`);
+    if (answerTwo === 'back') {
+        return;
+    }    
+    const chosenSyrup = Number(answerTwo) - 1; 
+    makingCoffee(beverage, chosenSyrup);
 }
 
 function fill() {
@@ -67,6 +99,9 @@ function fill() {
     supply.milk += Number(input('Write how many ml of milk you want to add: \n'));
     supply.beans += Number(input('Write how many grams of coffee beans you want to add: \n'));
     supply.cups += Number(input('Write how many disposable coffee cups you want to add: \n'));
+    syrups[0].amount += Number(input('Write how many ml of hazelnut syrup you want to add: \n'));
+    syrups[1].amount += Number(input('Write how many ml of almond syrup you want to add: \n'));
+    syrups[2].amount+= Number(input('Write how many ml of vanilla syrup you want to add: \n'));
 }
 
 function take() {
